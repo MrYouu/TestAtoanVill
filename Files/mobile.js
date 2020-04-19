8ec29bf8927324ff067ba597b33983f0e8a21b4b
Jim <51420306+MrYouu@users.noreply.github.com>
2020-04-19 15:27:11 +0300

    Add files via upload

--- Files/mobile.js
+++ Files/mobile.js
@@ -1,14 +1,11 @@
 function openNav()
 {
 	document.getElementById("mobile__menu").style.width = "100%";
-	document.getElementById("main").style.marginLeft = "100%";
-	document.getElementById("main").style.transition = "all .5s";
 	document.getElementById("main").style.overflowY = "hidden";
 }
 
 function closeNav()
 {
 	document.getElementById("mobile__menu").style.width = "0";
-	document.getElementById("main").style.marginLeft = "0";
 	document.getElementById("main").style.overflowY = "visible";
 }
